e0b019afb8ea15038e0415c8bc3b9bf0485f87d1
Patryk Konior <patrykkonior@MacBook-Pro-Patryk.local>
2026-01-07 13:23:53 +0100

    Add Req and Jason dependencies

--- mix.exs
+++ mix.exs
@@ -32,7 +32,9 @@ defmodule Sentry.MixProject do
   defp deps do
     [
       {:styler, "~> 1.2", only: [:dev, :test], runtime: false},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:req, "~> 0.5"}, # HTTP client (cos na wzor axios w js)
+      {:jason, "~> 1.4"} # JSON parser
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
