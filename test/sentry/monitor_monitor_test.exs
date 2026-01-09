e4380164d42fe5dbb9b95b77faedd105233ce68d
Patryk Konior <patrykkonior@MacBook-Pro-Patryk.local>
2026-01-09 13:08:39 +0100

    Add initial monitor test module #6

---
+++ test/sentry/monitor_monitor_test.exs
@@ -0,0 +1,19 @@
+defmodule Sentry.Monitor.MonitorTest do
+  @moduledoc """
+    Test dla modułu Sentry.Monitor.Monitor
+  """
+
+  use ExUnit.Case, async: true
+
+  describe "HTTP monitor" do
+    test "placeholder" do
+      assert true
+    end
+  end
+
+  describe "TCP monitor" do
+    test "placeholder" do
+      assert true
+    end
+  end
+end
