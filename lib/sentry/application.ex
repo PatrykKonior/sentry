dcba32a93ca3080d9b243612178aa87737fc4076
Patryk Konior <patrykkonior@MacBook-Pro-Patryk.local>
2026-01-09 09:30:34 +0100

    Create simple sentry, closes #5

--- lib/sentry/application.ex
+++ lib/sentry/application.ex
@@ -8,13 +8,32 @@ defmodule Sentry.Application do
   @impl true
   def start(_type, _args) do
     children = [
+      {Registry, keys: :unique, name: Sentry.Registry},
+      Sentry.Supervisor.Supervisor
       # Starts a worker by calling: Sentry.Worker.start_link(arg)
       # {Sentry.Worker, arg}
     ]
 
+    Supervisor.start_link(children,
+      strategy: :one_for_one,
+      name: Sentry.Application
+    )
+
+    # na sztywno wpisane endpointy
+    endpoints = [
+      %{url: "google.com", protocol: :http, port: 443, frequency: 5_000},
+      %{url: "example.com", protocol: :http, port: 80, frequency: 10_000},
+      %{url: "smtp.gmail.com", protocol: :tcp, port: 587, frequency: 15_000}
+    ]
+
+    endpoints
+    |> Enum.map(&Sentry.Endpoint.new/1)
+    |> Enum.each(&Sentry.Supervisor.Supervisor.start_monitor/1)
+
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Sentry.Supervisor]
-    Supervisor.start_link(children, opts)
+    # opts = [strategy: :one_for_one, name: Sentry.Application]
+    # Supervisor.start_link(children, opts)
+    {:ok, self()}
   end
 end
