4232022dccdf4e1868a9600c518efbc679d9413c
Patryk Konior <patrykkonior@MacBook-Pro-Patryk.local>
2026-01-12 15:54:05 +0100

    Finished tests for Sentry.Service.remove/1 #9

--- test/sentry/service_test.exs
+++ test/sentry/service_test.exs
@@ -27,4 +27,37 @@ defmodule Sentry.ServiceTest do
       assert {:ok, ^pid} = Store.get("localhost-add-test")
     end
   end
+
+  describe "remove/1" do
+    test "stops monitor and removes it from ETS" do
+      :ok = Store.init_table()
+
+      endpoint_params = %{
+        url: "localhost-remove-test",
+        protocol: :tcp,
+        port: 65_002,
+        frequency: 10
+      }
+
+      # najpierw dodajemy monitor
+      assert {:ok, pid} = Service.add(endpoint_params)
+      assert Process.alive?(pid)
+      assert {:ok, ^pid} = Store.get("localhost-remove-test")
+
+      # teraz usuwamy
+      assert :ok = Service.remove("localhost-remove-test")
+
+      # wpis w ETS powinien zniknąć
+      assert :error = Store.get("localhost-remove-test")
+
+      # proces monitora nie powinien już żyć
+      refute Process.alive?(pid)
+    end
+
+    test "returns :not_found when url is not present" do
+      :ok = Store.init_table()
+
+      assert {:error, :not_found} = Service.remove("non-existent-service")
+    end
+  end
 end
