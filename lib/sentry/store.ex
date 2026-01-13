541a6b1d5eabbad5f23e18120b17f20c75db7f30
Patryk Konior <patrykkonior@MacBook-Pro-Patryk.local>
2026-01-13 09:16:21 +0100

    Replaced ets with Nebulex, closes #10

--- lib/sentry/store.ex
+++ lib/sentry/store.ex
@@ -1,65 +1,52 @@
 defmodule Sentry.Store do
   @moduledoc """
-    Prosty wrapper na ETS do przechowywania mapowania endpoint URL -> pid monitora.
+    Prosty wrapper na Nebulex do przechowywania mapowania endpoint URL -> pid monitora.
   """
 
-  @doc """
-    1. ETS (Erlang Term Storage) to wbudowana, ultra szybka pamięć key–value w VM,
-    dostępna z dowolnego procesu, trzymana w pamięci RAM.
-    2. URL -> PID monitor i szybkie znajdywanie procesu po adresie
-  """
+  alias Sentry.Cache
 
-  @table :sentry_endpoints
-
-  # tutaj tworze tabelę ETS
+  # inicjalizacja tabeli
   @spec init_table() :: :ok
   def init_table do
-    # rozpoczynam od dowiedzenia się czy tabela o nazwie :sentry_endpoints w ogóle istnieje
-    # jak nie istnieje to niech zwraca undefined
-    case :ets.info(@table) do
-      # jak nie istnieje to chce zwrócić nową tabele
-      # :set - 1 klucz 1 wartość (jak map) :public - kazdy proces moze get i put do tej tabeli
-      # :named_table - tabela ma nazwe wiec nie trzymam jej id w zmiennej tylko atom
-      # read_concurrency: true – optymalizacja do szybkiego, równoległego czytania
-      :undefined ->
-        :ets.new(@table, [:set, :public, :named_table, read_concurrency: true])
-        :ok
-
-      # jak istnieje to po prostu nic nie robie - funkcja idempotentna
-      _info ->
-        :ok
-    end
+    # róznica jest taka, ze Nebulex sam inicjalizuje swoje struktury przy starcie Sentry.Cache
+    # tutaj musze zostawic API jako no-op (no operation) czyli funkcja, która realnie nic nie robi tylko, np. zwraca :ok
+    :ok
   end
 
   # to jest lista wszystkich wpisów
   @spec all() :: [{String.t(), pid()}]
   def all do
-    # zwracam cała zawartość tabeli jako listę {url, pid}
-    :ets.tab2list(@table)
+    # Cache.all() zwraca listę kluczy, np. ["one.example", "two.example", ...]
+    Cache.all()
+    |> Enum.map(fn url ->
+      case Cache.get(url) do
+        nil -> nil
+        pid -> {url, pid}
+      end
+    end)
+    |> Enum.reject(&is_nil/1)
   end
 
   # usunięcie wpisu po url -> pid
   @spec delete(String.t()) :: true
   def delete(url) do
-    :ets.delete(@table, url)
+    Cache.delete(url)
+    true
   end
 
   # odczyt po url
   @spec get(String.t()) :: {:ok, pid()} | :error
   def get(url) do
-    # tutaj zwracam listę dopasowanych wpisów albo pustą listę
-    case :ets.lookup(@table, url) do
-      # pierwszy pattern matching - ^url -> klucz musi by dokładnie tym samym url, który przekazałem w arg fn
-      # wyciągam pid i zwracam {:ok, pid}
-      [{^url, pid}] -> {:ok, pid}
-      # jak puste to error bo nic nie znalazłem
-      [] -> :error
+    case Cache.get(url) do
+      nil -> :error
+      pid -> {:ok, pid}
     end
   end
 
   # zapis url -> pid
   @spec put(String.t(), pid()) :: true
   def put(url, pid) do
-    :ets.insert(@table, {url, pid})
+    Cache.put(url, pid)
+    true
   end
 end
